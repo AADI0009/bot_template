e4fdba79582ebec79291170fcd2e6d7cd1392dc1
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 20:40:45 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 const client = require("../bot");
-const { prefix } = require("../config.json");
+const app.prefix = require("../config.js");
 
 console.log(`${client.user.username} ready!`);
 client.on("ready", async () => {
