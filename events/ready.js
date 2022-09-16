fb349f2f1a0dde64f05e9fef2d8fa5818a091fe0
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 18:55:43 +0530

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+const client = require("../index");
+const { prefix } = require("../config.json");
+
+console.log(`${client.user.username} ready!`);
+client.on("ready", async () => {
+  client.user.setActivity(
+    `${prefix}help`,
+    { type: "PLAYING" }
+  );
