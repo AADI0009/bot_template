4dbcf1a1ea1d634026b615280dc3ad600d93fedc
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 23:13:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const { Client, Intents } = require('discord.js');
+const { QuickDB } = require("quick.db");
+const db = new QuickDB({ filePath: "/root/bot_template/database.sqlite" });
 
 global.client = new Client({
     intents: [
