95c7abb67950234c58383397c4e9419c38164773
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 20:12:50 +0530

    Create warnings.js

---
+++ commands/warnings.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { emerald } = require("../colors.js");
+
+module.exports = {
+    name: 'warnings',
+
+    execute(client, message, args) {
+    message.delete();
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]);
+
+    if (!Member) return message.channel.send(`Please Mention A User!`);
+
+    let Warnings = client.db.get(
+      `Warnings_${message.guild.id}_${Member.user.id}`
+    );
+
+    let embed = new MessageEmbed()
+      .setColor(emerald)
+      .setTitle(`Member Warnings!`)
+      .setDescription(`${Member.user.username} Has ${Warnings || "0"} Warnings!`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+};
