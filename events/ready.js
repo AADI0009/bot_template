31583bd045aa4ef0cde300145dbed8714220dbea
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 21:02:59 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,1 @@
-const client = require("../bot");
-const app.prefix = require("../config.js");
-
-console.log(`${client.user.username} ready!`);
-client.on("ready", async () => {
-  client.user.setActivity(
-    `${prefix}help`,
-    { type: "PLAYING" }
-  );
-}
+module.exports = (client) => console.log(`${client.user.username} ready!`);
