b4dc41c7b1abd010b5560a641965b8794d3253f3
aadiyouknow <72033983+AADI0009@users.noreply.github.com>
2022-09-16 21:03:47 +0530

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const { red } = require("../colors.js");
+const { red } = require("../colors.json");
 
 module.exports = {
     name: 'warn',
